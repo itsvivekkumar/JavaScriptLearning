146cae48ddabdb9974f21f94d4fb6b875d5e0202
itsvivekkumar <159131608+itsvivekkumar@users.noreply.github.com>
2025-04-05 09:44:50 +0530

    updated guess.js by doin code refactoring using function ..From line 14 t0 40

--- GuessNumberProject/guess.js
+++ GuessNumberProject/guess.js
@@ -11,53 +11,89 @@ const displayMessage=function(message){
     document.querySelector('.message').textContent=message;
 }
 
-
+//Code refactoring using function
 document.querySelector('.check').addEventListener("click",function(){
-     const guess=Number(document.querySelector('.guess').value);
-    //  console.log(guess,typeof guess);
-     
-     if(!guess){
-        console.log('No random value is present!!!')
-         document.querySelector('.message').textContent='No value present!!!'; 
+    const guess=Number(document.querySelector('.guess').value);
+    if(!guess){
+         console.log('No random value is present!!!')
+        document.querySelector('.message').textContent='No value present!!!'; 
      }
-     else if(guess < randomNumber){
-        console.log('you entered lower value !!!');
+     else if(guess!=randomNumber){
         if(score>1){
-        document.querySelector('.message').textContent='you entered lower value !!!';
-        score--;
-        document.querySelector('.score').textContent = score;
+            displayMessage( guess<randomNumber ? "you entered lower value !" : "you entered higher value !");
+            score--;
+            document.querySelector('.score').textContent = score;
         }
-        else{
-            console.log('you lost match!!'); 
-            document.querySelector('.message').textContent='you lost match!!';
+        else {
+            displayMessage("you lost match");
             document.querySelector('.score').textContent = 0;
         }
      }
-     else if(guess > randomNumber){
-        console.log('you entered higher value !!!');
-        if(score>1){
-            document.querySelector('.message').textContent='you entered higher value !!!';
-            score--;
-            document.querySelector('.score').textContent = score;
-            }
-            else{
-                console.log('you lost match!!'); 
-                document.querySelector('.message').textContent='you lost match!!';
-                document.querySelector('.score').textContent = 0;
-        }
-     }
-     else {
-        console.log('Correct answer You won !!! ');
-        document.querySelector('.message').textContent='Correct answer You won !!!';
+     else{
+        displayMessage('🎉 Correct answer You won !');
         document.querySelector('body').style.backgroundColor = '#60b347';
         document.querySelector('.number').style.width = '30rem';
-    
+
         if (score > highscore) {
-          highscore = score;
-          document.querySelector('.highscore').textContent = highscore;
-        } 
-     }
+            highscore = score;
+            document.querySelector('.highscore').textContent = highscore;
+        }
+    }
 })
+
+
+
+
+
+
+//Brute force approach code
+// document.querySelector('.check').addEventListener("click",function(){
+//      const guess=Number(document.querySelector('.guess').value);
+//     //  console.log(guess,typeof guess);
+     
+//      if(!guess){
+//         console.log('No random value is present!!!')
+//          document.querySelector('.message').textContent='No value present!!!'; 
+//      }
+//      else if(guess < randomNumber){
+//         console.log('you entered lower value !!!');
+//         if(score>1){
+//         document.querySelector('.message').textContent='you entered lower value !!!';
+//         score--;
+//         document.querySelector('.score').textContent = score;
+//         }
+//         else{
+//             console.log('you lost match!!'); 
+//             document.querySelector('.message').textContent='you lost match!!';
+//             document.querySelector('.score').textContent = 0;
+//         }
+//      }
+//      else if(guess > randomNumber){
+//         console.log('you entered higher value !!!');
+//         if(score>1){
+//             document.querySelector('.message').textContent='you entered higher value !!!';
+//             score--;
+//             document.querySelector('.score').textContent = score;
+//             }
+//             else{
+//                 console.log('you lost match!!'); 
+//                 document.querySelector('.message').textContent='you lost match!!';
+//                 document.querySelector('.score').textContent = 0;
+//         }
+//      }
+//      else {
+//         console.log('Correct answer You won !!! ');
+//         document.querySelector('.message').textContent='Correct answer You won !!!';
+//         document.querySelector('body').style.backgroundColor = '#60b347';
+//         document.querySelector('.number').style.width = '30rem';
+    
+//         if (score > highscore) {
+//           highscore = score;
+//           document.querySelector('.highscore').textContent = highscore;
+//         } 
+//      }
+// })
+
 
 document.querySelector('.again').addEventListener('click',function(){
     console.log(document.querySelector('.number').textContent);
@@ -68,6 +104,8 @@ document.querySelector('.again').addEventListener('click',function(){
     document.querySelector('.highscore').textContent = highscore;
     document.querySelector('.number').textContent =randomNumber;
     document.querySelector('.guess').value = '';
+    document.querySelector('body').style.backgroundColor ='#344';
+    document.querySelector('.number').style.width = '15rem';
 
 })
  
